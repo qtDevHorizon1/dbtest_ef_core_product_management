d6a53e50d33ed15a02442c56e09784cf1f8dc180
awsuser <root@ip-10-0-4-152.ec2.internal>
2025-09-24 19:18:56 +0000

    AWS Transform DB Mod commit for jobId 6c42cd2e-de8c-4de4-b92d-254944a0752e

--- Models/ProductStats.cs
+++ Models/ProductStats.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFCore.Models
 {
-    [Table("ProductStats")]
+    [Table("productstats", Schema = "productmanagement_dbo")]
     public class ProductStats
     {
         [Key]
